b0e4e9387624f900d6c17ecc9123c4a4fd1a3b39
brandonmcghee <brandonmcghee@fullsail.edu>
2013-04-11 21:30:14 -0400

    Week 1 Commit

---
+++ Week 1/Android/assets/www/js/main.js
@@ -0,0 +1,19 @@
+//Brandon McGhee
+//AVF 1304
+
+/////PAGE LISTENERS/////
+
+//Home Page Listener
+$('#home').on('pageinit', function(){
+              
+    //Do whatever is needed
+              
+});
+
+
+                 
+
+
+
+
+
